95f62dfe1e5647d2da0ba0ee4aed51e40e047f45
YinChang <yinchang0626@gmail.com>
2025-03-13 10:18:24 +0800

    modify file

--- HomeWork/Git_GitHub版本控制與協作練習/C110152234/ascii.py
+++ HomeWork/Git_GitHub版本控制與協作練習/C110152234/ascii.py
@@ -2,19 +2,7 @@ for times in range (3):
     wordl = sorted(input(" "))
     w = 0
     
-    for j in wordl:
-        i = 97
-        wascii =int(ord(j))
-        long = wordl.count(j)
-        
-        for i in range(i,123):
-            if(i != w):
-                if (wascii == i):
-                    print(chr(i))
-                    w = i 
-                    for p in range(0,long):
-                        print("*",end="" )
-                    print('\n')
+
                 
     for a in wordl:
         I = 65
