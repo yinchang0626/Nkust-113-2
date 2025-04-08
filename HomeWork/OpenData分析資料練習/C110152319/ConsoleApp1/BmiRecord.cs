c78f05860cf919553935cff4d4396e44cd9e40f9
Jen-Fu <71171126+JenFuChen@users.noreply.github.com>
2025-04-08 17:47:59 +0800

    Upload hw2

---
+++ HomeWork/OpenData分析資料練習/C110152319/ConsoleApp1/BmiRecord.cs
@@ -0,0 +1,25 @@
+﻿// CSV 列與 BmiRecord 類屬性之間的映射
+using CsvHelper.Configuration;
+
+public class BmiRecordMap : ClassMap<BmiRecord>
+{
+    public BmiRecordMap()
+    {
+        Map(m => m.年).Name("年");
+        Map(m => m.月).Name("月");
+        Map(m => m.BMI值體位).Name("BMI值體位");
+        Map(m => m.體位).Name("體位");
+        Map(m => m.人數).Name("人數");
+        Map(m => m.百分比).Name("百分比");
+    }
+}
+
+public class BmiRecord
+{
+    public string 年 { get; set; } // 年
+    public string 月 { get; set; } // 月
+    public string BMI值體位 { get; set; } // BMI值體位
+    public string 體位 { get; set; } // 體位
+    public int 人數 { get; set; } // 人數
+    public string 百分比 { get; set; } // 百分比
+}
