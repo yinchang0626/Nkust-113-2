99f1ed51164b52d8a950c98dfcc14574a0b5c33e
lzx_ovo <lzx2003227@gmail.com>
2025-05-25 21:00:36 +0800

    移動整體程式至作業資料夾

---
+++ HomeWork/AspNetCoreMvc基礎結構認知/C111152301/Crime/Crime/Controllers/ImportController.cs
@@ -0,0 +1,38 @@
+﻿using Crime.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crime.Controllers
+{
+    [Route("Import")]
+    public class ImportController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly CsvService _csvService;
+        private readonly IWebHostEnvironment _env;
+
+        public ImportController(ApplicationDbContext context, CsvService csvService, IWebHostEnvironment env)
+        {
+            _context = context;
+            _csvService = csvService;
+            _env = env;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+                int count = await _csvService.ImportCsvAsync(stream);
+                ViewBag.Message = $"成功匯入 {count} 筆資料";
+            }
+            return View();
+        }
+    }
+}
---
+++ HomeWork/AspNetCoreMvc基礎結構認知/C111152301/Crime/Crime/Models/CrimeStat.cs
@@ -0,0 +1,18 @@
+﻿public class CrimeStat
+{
+    public int Id { get; set; }
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public int TotalCases { get; set; }
+    public int RapeCases { get; set; }
+    public int RobberyCases { get; set; }
+    public int OtherCases { get; set; }
+    public double IncidentRate { get; set; }
+    public int Suspects { get; set; }
+    public double CrimeRate { get; set; }
+    public int SolvedTotal { get; set; }
+    public int SolvedCurrent { get; set; }
+    public int SolvedBacklog { get; set; }
+    public int SolvedOther { get; set; }
+    public double SolveRate { get; set; }
+}
