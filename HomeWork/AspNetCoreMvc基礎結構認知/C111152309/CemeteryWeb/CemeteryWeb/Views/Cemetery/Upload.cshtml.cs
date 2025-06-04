db1d5379fcb8bc8c2553546054b4f7db72e92ade
system192 <bg930120@gmail.com>
2025-06-05 00:16:53 +0800

    作業上傳

---
+++ HomeWork/AspNetCoreMvc基礎結構認知/C111152309/CemeteryWeb/CemeteryWeb/Views/Cemetery/Upload.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CemeteryWeb.Views.Cemetery
+{
+    public class UploadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
