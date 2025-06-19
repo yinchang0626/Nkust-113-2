8ffd4d2b99c3bae1cc9652b337638ad2d0fce9c8
c110110118 <c110110118@nkust.edu.tw>
2025-06-19 21:00:18 +0800

    C110110118

---
+++ HomeWork/AspNetCoreMvc基礎結構認知/C110110118/C110110118/C110110118/Kcg/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kcg.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
+
